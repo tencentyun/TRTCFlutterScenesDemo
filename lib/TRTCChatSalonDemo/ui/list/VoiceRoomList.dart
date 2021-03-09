d6151bc9b3beccb9119ef602e363e29f47707a6d
gavinwjwang <gavinwjwang@tencent.com>
2021-03-09 18:11:05 +0800

    WillPopScope

--- lib/TRTCChatSalonDemo/ui/list/VoiceRoomList.dart
+++ lib/TRTCChatSalonDemo/ui/list/VoiceRoomList.dart
@@ -125,120 +125,129 @@ class VoiceRoomListPageState extends State<VoiceRoomListPage> {
               },
             ),
           ]),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            stops: [0.0, 1.0],
-            colors: [
-              Color.fromRGBO(19, 41, 75, 1),
-              Color.fromRGBO(0, 0, 0, 1),
-            ],
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.pushReplacementNamed(
+            context,
+            "/index",
+          );
+          return true;
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 1.0],
+              colors: [
+                Color.fromRGBO(19, 41, 75, 1),
+                Color.fromRGBO(0, 0, 0, 1),
+              ],
+            ),
           ),
-        ),
-        child: EasyRefresh(
-          emptyWidget: roomCount <= 0
-              ? Center(
-                  child: Text(
-                    '暂无语音沙龙',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                )
-              : null,
-          onRefresh: () async {
-            print('onRefresh');
-            getRoomList();
-          },
-          child: CustomScrollView(
-            slivers: [
-              SliverPadding(
-                padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
-                sliver: SliverGrid(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, //Grid按两列显示
-                    mainAxisSpacing: 20.0,
-                    crossAxisSpacing: 20.0,
-                    childAspectRatio: 1.0,
-                  ),
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      RoomInfo info = roomInfList[index];
-                      return InkWell(
-                        onTap: () {
-                          goRoomInfoPage(info);
-                        },
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            Image.network(
-                              info.coverUrl != null && info.coverUrl != ''
-                                  ? info.coverUrl
-                                  : TxUtils.getRandoAvatarUrl(),
-                              fit: BoxFit.fitWidth,
-                            ),
-                            Positioned(
-                              left: 10,
-                              right: 8,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    info.roomName == null
-                                        ? "无主题"
-                                        : info.roomName,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18),
-                                  ),
-                                ],
+          child: EasyRefresh(
+            emptyWidget: roomCount <= 0
+                ? Center(
+                    child: Text(
+                      '暂无语音沙龙',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                : null,
+            onRefresh: () async {
+              print('onRefresh');
+              getRoomList();
+            },
+            child: CustomScrollView(
+              slivers: [
+                SliverPadding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
+                  sliver: SliverGrid(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, //Grid按两列显示
+                      mainAxisSpacing: 20.0,
+                      crossAxisSpacing: 20.0,
+                      childAspectRatio: 1.0,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        RoomInfo info = roomInfList[index];
+                        return InkWell(
+                          onTap: () {
+                            goRoomInfoPage(info);
+                          },
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Image.network(
+                                info.coverUrl != null && info.coverUrl != ''
+                                    ? info.coverUrl
+                                    : TxUtils.getRandoAvatarUrl(),
+                                fit: BoxFit.fitWidth,
                               ),
-                            ),
-                            Positioned(
-                              bottom: 10,
-                              left: 8,
-                              width: 90,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    info.ownerName == null
-                                        ? "--"
-                                        : info.ownerName,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.white,
+                              Positioned(
+                                left: 10,
+                                right: 8,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      info.roomName == null
+                                          ? "无主题"
+                                          : info.roomName,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 18),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 10,
-                              right: 8,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    info.memberCount.toString() + '人在线',
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.white,
+                              Positioned(
+                                bottom: 10,
+                                left: 8,
+                                width: 90,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      info.ownerName == null
+                                          ? "--"
+                                          : info.ownerName,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                    childCount: roomCount,
+                              Positioned(
+                                bottom: 10,
+                                right: 8,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      info.memberCount.toString() + '人在线',
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                      childCount: roomCount,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
